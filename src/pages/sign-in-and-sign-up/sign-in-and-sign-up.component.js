7f3ac0d6f51a4e085646cc7bf0087ed1c2688af9
kirankumarrr <kirankumarrr>
2021-10-08 15:59:06 +0530

    Finished sign in and sign up authentication using firebase

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.js
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.js
@@ -3,11 +3,15 @@ import React from 'react';
 import './sign-in-and-sign-up.component';
 import '../../components/sign-in/sign-in.component';
 import SignIn from '../../components/sign-in/sign-in.component';
+import SignUp from '../../components/sign-up/sign-up.component';
+
+import './sign-in-and-sign-up.styles.scss';
 
 const SignInSignUpPage = () => {
   return (
     <div className="sign-in-and-sign-up">
       <SignIn />
+      <SignUp />
     </div>
   );
 };
